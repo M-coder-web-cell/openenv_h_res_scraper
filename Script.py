3a686cbb613d3ae749fc622241af7c9da5ec7a57
HackRogue <mazumdarh549@gmail.com>
2026-05-01 01:00:15 +0530

    download the space only if created after 15th

--- Script.py
+++ Script.py
@@ -17,21 +17,22 @@ hf_api = HfApi(
 )
 
 spaces = hf_api.list_spaces(filter = ["openenv"], limit = 4)
+
 spaces_id = []
 threshold_date = datetime(2026, 3, 15, tzinfo=timezone.utc)
 for space in spaces:
+    #fetch all repos spaces created after 15th march
     if(space.created_at > threshold_date):
-        print("True")
-    spaces_id.append(space.id)
+        spaces_id.append(space.id)
 
 #EXTRACT THE READMES AND STORE THEM UNDER THE DATA FOLDER FOR FUTURE PROCESSING
-#root = Path(__file__).resolve().parent
-#data_folder = root / "data"
-#
-#for spaceid in spaces_id:
-#    downloaded_path = hf_api.hf_hub_download(repo_id = spaceid, repo_type="space", filename = "README.md", local_dir = data_folder)
-#    dst_safe_name = spaceid.replace("/", "_") + ".md"
-#    dst = data_folder / dst_safe_name 
-#    Path(downloaded_path).replace(dst)
+root = Path(__file__).resolve().parent
+data_folder = root / "data"
+
+for spaceid in spaces_id:
+    downloaded_path = hf_api.hf_hub_download(repo_id = spaceid, repo_type="space", filename = "README.md", local_dir = data_folder)
+    dst_safe_name = spaceid.replace("/", "_") + ".md"
+    dst = data_folder / dst_safe_name 
+    Path(downloaded_path).replace(dst)
 
     
